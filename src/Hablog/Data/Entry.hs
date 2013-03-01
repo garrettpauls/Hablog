9c4c2b6168cfe2a24976c764a981b8abfbab57fe
Garrett Pauls <garrett.pauls@gmail.com>
2013-02-28 19:51:50 -0700

    Added category and user models

--- src/Hablog/Data/Entry.hs
+++ src/Hablog/Data/Entry.hs
@@ -1,16 +1,43 @@
 {-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeFamilies, OverloadedStrings, GADTs, FlexibleContexts #-}
 module Hablog.Data.Entry where
 
+import Data.ByteString
 import Data.Text
+import Data.Time.Clock
 import Database.Persist
 import Database.Persist.TH
 import Hablog.Data.Slug (Slug)
 
+data MarkupEngine = Markdown deriving (Show, Read, Eq)
+derivePersistField "MarkupEngine"
+
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persist|
-Entry
+Category
   slug  Slug
   title Text
-  UniqueSlug slug
+  UniqueCategory slug
+  deriving Show
+
+User
+  userName    Text
+  password    ByteString
+  email       Text
+  displayName Text
+  UniqueUserName userName
+  deriving Show
+
+Entry
+  slug         Slug
+  title        Text
+  authorId     UserId
+  categoryId   Category Maybe
+  markup       Text
+  markupEngine MarkupEngine
+  renderedHtml Text
+  published    Bool
+  publishDate  UTCTime
+  updateDate   UTCTime
+  UniqueEntry slug
   deriving Show
 |]
 
